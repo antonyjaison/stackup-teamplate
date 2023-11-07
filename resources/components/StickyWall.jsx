400ec052ef6b69cba29c6163d3a8c817e86bb638
Antony Jaison <antonyjaison639@gmail.com>
2023-11-08 00:00:57 +0530

    added content to stickywall component

--- resources/components/StickyWall.jsx
+++ resources/components/StickyWall.jsx
@@ -2,7 +2,16 @@ import styles from '@styles/stickywall.module.scss'
 
 const StickyWall = () => {
   return (
-    <div className={styles.wrapper}>StickyWall</div>
+    <div className={styles.wrapper}>
+      <h3 className={styles.task_heading}>Social media</h3>
+      <div className={styles.tasks}>
+        <p>- plan a socicl media content</p>
+        <p>- plan socicl content</p>
+        <p>- plan a socicl media content</p>
+        <p>- plan a socicl</p>
+        <p>- plan a socicl media content</p>
+      </div>
+    </div>
   )
 }
 
