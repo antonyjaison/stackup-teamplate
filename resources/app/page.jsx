013a9f5612e4ecb626f02bae722e1a2afa18ccad
Aarsha C P <aarshasubash7@gmail.com>
2023-11-08 21:56:27 +0530

    added homepage

--- resources/app/page.jsx
+++ resources/app/page.jsx
@@ -1,9 +1,34 @@
-import styles from '@styles/homepage.module.scss'
+import styles from "@styles/homepage.module.scss";
+import Link from "next/link";
 
 export default function Home() {
-  return(
+  return (
     <>
-    hello
+      <div className={styles.wrapper}>
+        <div className={styles.dim_layer}>
+          <nav>
+            <img src="/images/timanage.svg" alt="logo" />
+            <Link href="/login">Login</Link>
+          </nav>
+
+          <div className={styles.content}>
+            <h1>
+              Task <br />
+              <span>Management</span>
+            </h1>
+            <p>
+              Task management entails structuring and organizing tasks
+              efficiently, employing strategies to prioritize, execute, and
+              track progress, ultimately ensuring effective and timely
+              completion.
+            </p>
+          </div>
+
+          <div></div>
+          <div></div>
+
+        </div>
+      </div>
     </>
-  )
+  );
 }
